912c6fd763812a02286c5c7d7774e18cc87b2621
CamillyLee <camillyschmitz7@gmail.com>
2026-08-31 16:22:26 -0300

    fix: corrige imports do Header

--- frontend/src/pages/NovaViagem.jsx
+++ frontend/src/pages/NovaViagem.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
-import Header from "../components/Header";
+import Header from "../components/Header.jsx";
 import Navbar from "../components/Navbar";
 import TabelaDespesas from "../components/TabelaDespesas";
 
